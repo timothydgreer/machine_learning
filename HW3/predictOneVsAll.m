c2597d53eb3056618a00f581f9966c9cf7b4bf37
timothydgreer <timothydgreer@gmail.com>
2015-12-23 14:57:08 -0500

    Update predictOneVsAll.m

--- HW3/predictOneVsAll.m
+++ HW3/predictOneVsAll.m
@@ -29,17 +29,13 @@ X = [ones(m, 1) X];
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
 %       
-size(all_theta);
-size(X);
-a = sigmoid(X*all_theta')
-m;
-size(a)
+a = sigmoid(X*all_theta');
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 for i = 1:m
     [y,j] = max(a(i,:));
-    p(i) = j;
+    p(i) = j
     
 
 
